aef8cf47832e234a1d9378ff09a3f0447199fc1f
Kulakov619 <kulakov_i_v@school619.ru>
2025-06-05 09:26:15 +0300

    init

--- core/example_posts/views.py
+++ core/example_posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class TestPage(TemplateView):
+    template_name = 'example_posts/test_page.html'
+
